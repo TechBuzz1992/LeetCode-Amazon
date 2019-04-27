d24d92b4f73e9e0f5d7772722ce7ce7f9b99a0a3
harsh.dusane <harsh.dusane@AABRD0376.AASPL-BRD.COM>
2019-04-27 19:27:54 +0530

    commited

--- src/com/Amazon/Easy/MinCostClimbingStairs.java
+++ src/com/Amazon/Easy/MinCostClimbingStairs.java
@@ -4,7 +4,18 @@ public class MinCostClimbingStairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+		System.out.println(minCostClimbingStairs(arr));
 
 	}
+	public static int minCostClimbingStairs(int[] cost) {
+        int f1 = 0, f2 = 0;
+        for (int i = cost.length - 1; i >= 0; --i) {
+            int f0 = cost[i] + Math.min(f1, f2);
+            f2 = f1;
+            f1 = f0;
+        }
+        return Math.min(f1, f2);
+    }
 
 }
